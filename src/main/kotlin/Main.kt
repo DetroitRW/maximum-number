4ddf613be21ed2d63dc05646995d0a1bec2d6a2a
Ilya Brant <detroitib31@gmail.com>
2022-11-02 17:30:39 +0600

    Написал поле поле для ввода данных

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,12 @@ import java.util.*
 fun main(args: Array<String>) {
 
     val reader = Scanner(System.`in`)
+    print("Введите число: ")
+    var a = reader.nextInt()
+
+    print("Введите число: ")
+    var b = reader.nextInt()
+
+    print("Введите число: ")
+    var c = reader.nextInt()
 }
